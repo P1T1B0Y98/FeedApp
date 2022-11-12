3ea0fb2a0a79dc4929cb6e9abb73b0c80eff7c2c
Malin Iversen <malinive@hotmail.com>
2022-11-12 16:48:10 +0100

    answer and result not finished!!

--- frontend/src/components/Poll/Poll-Answer/answerPoll.tsx
+++ frontend/src/components/Poll/Poll-Answer/answerPoll.tsx
@@ -1,23 +1,91 @@
 import "./answerPoll.css";
 import { SiJavascript } from "solid-icons/si";
 import { Icon } from "solid-heroicons";
+import { useNavigate, NavLink } from '@solidjs/router';
+import { createSignal, createEffect, createMemo, onMount } from "solid-js";
 
 
 
-const polls_JSON = '{"Id":"0c761621-7c3e-4b4f-90ae-028cadf0a817", "Question": "Is this a poll?", "IsPrivate":true, "IsClosed":false, "EndTime":"2022-11-04 18:37:26.997+00", "CreatorId":"cd12d700-030a-4ef7-a836-e8e99d28a00d"}';
-const polls_Obj = JSON.parse(polls_JSON);
-
-const votes_JSON = '{"Id":"0c761621-7c3e-4b4f-90ae-028cadf0a817", "total_votes":"500", "yes_votes":"190", "no_votes":"310"}';
-const votes_Obj = JSON.parse(votes_JSON);
 
 function AnswerPoll() {
+
+    const [countVotes, setCountVotes] = createSignal(0);
+    const [positiveVotes, setPositiveVotes] = createSignal(0);
+    const [negativeVotes, setNegativeVotes] = createSignal(0);
+    const [endTime, setEndTime] = createSignal('');
+    const [question, setQuestion] = createSignal('');
+
+    const navigate = useNavigate();
+    fetch(`https://localhost:7280/api/poll/1039516104483041280`, {
+                method: 'GET',
+                mode: 'cors',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                },
+                
+                //body: JSON.stringify(data)
+            })
+            .then(response => {
+                if (response.status === 200) {
+                    console.log(response);
+                    // alert('Get request success');
+                    return response.json();
+                } else {
+                    alert('Get request success invalid'); 
+                }
+            }
+        )
+            .then(data => {
+                setCountVotes(data["countVotes"]);
+                setPositiveVotes( data["positiveVotes"]);
+                setNegativeVotes( data["negativeVotes"]);
+                setEndTime(data['endTime']);
+                setQuestion(data['question']);
+            })
+
+fetch(`https://localhost:7280/api/poll/1039516104483041280`, {
+                method: 'PUT',
+                mode: 'cors',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                },
+                
+                body: JSON.stringify({
+                    "id": "1039516104483041280",
+                    "question": "Suger petter?",
+                    "isPrivate": false,
+                    "isClosed": false,
+                    "endTime": "2022-11-15T12:25:33.891Z",
+                    "creatorId": "d9e13ff8-e823-48b7-85e6-eb667466e6fe",
+                    "creatorName": "jolkohol",
+                    "countVotes": 5,
+                    "positiveVotes": 2,
+                    "negativeVotes": 3
+                    })
+            })
+            .then(response => {
+                if (response.status === 200) {
+                    console.log(response);
+                    // alert('Get request success');
+                    return response.json();
+                } else {
+                    alert('Get request success invalid'); 
+                }
+            }
+        )
+
+        .then(json => console.log(json))
+
+
     return (
         
         <div class="trial">
             <div class="poll-answer">
                 <form class="poll-answer-form" action="">
                     <div class ="all-elements">
-                    <h3 class="poll-question">{polls_Obj.Question}</h3>
+                    <h3 class="poll-question">{question}</h3>
 
                     
                     <div class="radio-buttons">
@@ -33,7 +101,7 @@ function AnswerPoll() {
 
 
                     <div class="text-and-button">
-                        <p id="text"> {votes_Obj.total_votes} overall votes  |  Poll closes at {polls_Obj.EndTime} </p>
+                        <p id="text"> Poll closes at {endTime} </p>
                         <input class="submit-answer-btn"  type="submit" value="Vote"></input>
                     </div>
                     </div>
--- frontend/src/components/Poll/Poll-Results/resultPoll.tsx
+++ frontend/src/components/Poll/Poll-Results/resultPoll.tsx
@@ -1,6 +1,7 @@
 import "./resultPoll.css";
-import { SiJavascript } from "solid-icons/si";
-import { Icon } from "solid-heroicons";
+import { useNavigate, NavLink } from '@solidjs/router';
+import { createSignal, createEffect, createMemo, onMount } from "solid-js";
+
 
 const polls_JSON = '{"Id":"0c761621-7c3e-4b4f-90ae-028cadf0a817", "Question": "Is this a poll?", "IsPrivate":true, "IsClosed":false, "EndTime":"2022-11-04 18:37:26.997+00", "CreatorId":"cd12d700-030a-4ef7-a836-e8e99d28a00d"}';
 const polls_Obj = JSON.parse(polls_JSON);
@@ -12,26 +13,75 @@ const votes_Obj = JSON.parse(votes_JSON);
 
 
 function ResultPoll() {
+
+    const [countVotes, setCountVotes] = createSignal(0);
+    const [positiveVotes, setPositiveVotes] = createSignal(0);
+    const [negativeVotes, setNegativeVotes] = createSignal(0);
+    const [endTime, setEndTime] = createSignal('');
+    const [question, setQuestion] = createSignal('');
+
+
+
+    //const token = localStorage.getItem("token");
+    //var authentic = token?.substring(1, token.length-1);
+
+    const token = localStorage.getItem("token");
+    var authentic = token?.substring(1, token.length-1);
+    const navigate = useNavigate();
+    fetch(`https://localhost:7280/api/poll/1039516104483041280`, {
+                method: 'GET',
+                mode: 'cors',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                },
+                
+                //body: JSON.stringify(data)
+            })
+            .then(response => {
+                if (response.status === 200) {
+                    console.log(response);
+                    // alert('Get request success');
+                    return response.json();
+                } else {
+                    alert('Get request success invalid'); 
+                }
+            }
+        )
+            .then(data => {
+                setCountVotes(data["countVotes"]);
+                setPositiveVotes( data["positiveVotes"]);
+                setNegativeVotes( data["negativeVotes"]);
+                setEndTime(data['endTime']);
+                setQuestion(data['question']);
+            })
+
+
+
+
     return (
         
         <div class="trial">
             <div class="poll-result">
                 <form class="poll-result-form" action="">
                     <div class ="all-elements">
-                    <h3 class="poll-question">{polls_Obj.Question}</h3>
+                    <h3 class="poll-question">{question}</h3>
 
                     <div class="container">
-                    <div class="result yes" data-width={(votes_Obj.yes_votes)/(votes_Obj.total_votes)*100}> {(votes_Obj.yes_votes)/(votes_Obj.total_votes)*100}%</div>
+                    <progress id="result yes" value={(votes_Obj.yes_votes)/(votes_Obj.total_votes)*100} max="100"> {(votes_Obj.no_votes)/(votes_Obj.total_votes)*100}%</progress>
                     </div>
   
-                    <div class="container" >
-                    <div class="result no" data-width={(votes_Obj.no_votes)/(votes_Obj.total_votes)*100}>{(votes_Obj.no_votes)/(votes_Obj.total_votes)*100}%</div> 
+                    <div class="container">
+                    <progress id="result no" value={(votes_Obj.no_votes)/(votes_Obj.total_votes)*100} max="100"> {(votes_Obj.no_votes)/(votes_Obj.total_votes)*100}%</progress>
                     </div>
-                    
+
+                     <div class="total-votes">{countVotes} total votes</div>
+                     <div class="pos-votes">{positiveVotes} positive votes</div>
+                     <div class="neg-votes">{negativeVotes} negative votes</div>
 
                         
                     <div class="text-and-button">
-                        <p id="text"> {votes_Obj.total_votes} overall votes  |  Poll closes at {polls_Obj.EndTime} </p>
+                        <p id="text"> {countVotes} overall votes  |  Poll closes at {endTime} </p>
                         <input class="submit-answer-btn"  type="submit" disabled value="Vote"></input>
                     </div>
                     
